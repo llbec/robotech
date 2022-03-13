458caa5e1245143de82bd43b33b546d7990e6afe
llbec <30335141+bicliu@users.noreply.github.com>
2022-03-13 21:23:58 +0800

    update withdraw cmd

--- mixedforce/withdraw/xcomcredit/cmd.go
+++ mixedforce/withdraw/xcomcredit/cmd.go
@@ -13,7 +13,7 @@ func initCmd() {
 	flag.BoolVar(&fRun, "r", false, "running daemon")
 	flag.BoolVar(&fTerminate, "t", false, "Terminate fallback process. default false")
 	flag.BoolVar(&fInit, "i", false, "Generat config file. default false")
-	flag.BoolVar(&fInit, "s", false, "running simple mod")
+	flag.BoolVar(&fSimple, "s", false, "running simple mod")
 }
 
 func parseCmd() {
