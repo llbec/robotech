565ba81c8db1892ab619cfd63d5e37e521cda62b
llbec <klins@163.com>
2026-01-08 00:27:03 +0800

    update

--- squadrons/ether-indexer/cmd/main.go
+++ squadrons/ether-indexer/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"ether-indexer/internal/storage"
@@ -13,8 +14,12 @@ import (
 func main() {
 
 	var dataPath string
-	flag.StringVar(&dataPath, "p", "data", "project db directory")
+	flag.StringVar(&dataPath, "p", ".data", "project db directory")
 	flag.Parse()
+
+	if err := os.MkdirAll(dataPath, 0755); err != nil {
+		log.Fatal("create data directory failed:", err)
+	}
 
 	dbPath := filepath.Join(dataPath, "indexer.db")
 	fmt.Println("Using DB:", dbPath)
